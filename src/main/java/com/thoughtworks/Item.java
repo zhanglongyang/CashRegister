7df8c22b90621c8bb4fa9d601e281e9e762f67d7
Longyang Zhang <zhanglongyang.cn@gmail.com>
2016-02-28 07:52:48 +0800

    add showing payment list implementation in Item model for now

--- src/main/java/com/thoughtworks/Item.java
+++ src/main/java/com/thoughtworks/Item.java
@@ -1,13 +1,20 @@
 package com.thoughtworks;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by lyzhang on 2/25/16.
  */
 public class Item {
+    private static final Integer DISCOUNT = 95;
+
     private String barCode;
     private String name;
+    private String unit;
     private Double price;
     private Integer count;
+    private Boolean hasGift;
+    private Boolean hasDiscount;
 
     public void setBarCode(String barCode) {
         this.barCode = barCode;
@@ -25,6 +32,14 @@ public class Item {
         this.name = name;
     }
 
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
     public Double getPrice() {
         return price;
     }
@@ -40,4 +55,62 @@ public class Item {
     public void setCount(Integer count) {
         this.count = count;
     }
+
+    public Boolean hasGift() {
+        if (hasGift == null) {
+            hasGift = false;
+        }
+
+        return hasGift;
+    }
+
+    public void setHasGift(Boolean hasGift) {
+        this.hasGift = hasGift;
+    }
+
+    public Boolean hasDiscount() {
+        if (hasDiscount == null) {
+            hasDiscount = false;
+        }
+
+        return hasDiscount;
+    }
+
+    public void setHasDiscount(Boolean hasDiscount) {
+        this.hasDiscount = hasDiscount;
+    }
+
+    public Double calculateSubtotal() {
+        if (hasGift()) {
+            return (getCount() - getCount() / 3) * getPrice();
+        }
+
+        if (hasDiscount()) {
+            return getPrice() * getCount() * DISCOUNT / 100;
+        }
+
+        return getPrice() * getCount() * 100 / 100;
+    }
+
+    public Double calculateCutDown() {
+        return getPrice() * getCount() - calculateSubtotal();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        DecimalFormat df = new DecimalFormat("0.00");
+        sb.append("名称：").append(getName()).append("，");
+        sb.append("数量：").append(getCount()).append(getUnit()).append("，");
+        sb.append("单价：").append(df.format(getPrice())).append("(元)").append("，");
+        sb.append("小计：").append(df.format(calculateSubtotal())).append("(元)");
+
+        if(hasDiscount()) {
+            sb.append("，");
+            sb.append("节省").append(df.format(calculateCutDown())).append("(元)");
+        }
+
+        return sb.toString();
+    }
+
 }
